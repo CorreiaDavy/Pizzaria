43d1fac54258d402c7de5c77bd0e25da4677af78
CorreiaDavy <164753449+CorreiaDavy@users.noreply.github.com>
2024-05-05 20:12:44 -0300

    Site Pizzaria
    Foi criado o site de uma pizzaria, adicionado os produtos, cabeçalho, e começou a desenvolver o carrinho de compras.

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const adici_car_bota = document.getElementsByClassName("butao_adicio_car");
+for(var i=0; i<adici_car_bota.length;i++){
+    adici_car_bota[i].addEventListener("click", function (event){
+        event.target.parentElement
+    })
+}
